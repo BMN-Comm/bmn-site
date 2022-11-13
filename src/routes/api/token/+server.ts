518d03f36e51a4098388a0dd9522665b2dc7b238
Rienk <rienkfidder@gmail.com>
2022-11-13 13:00:35 +0100

    Made sure token stays valid

--- src/routes/api/token/+server.ts
+++ src/routes/api/token/+server.ts
@@ -28,7 +28,10 @@ export const POST: RequestHandler = async (event) => {
 				),
 				{
 					path: '/',
-					httpOnly: true
+					httpOnly: true,
+					// Make sure the token only expires after a year
+					// (Keep in mind this does mean that people need to log out and in if for example the custom claims have changed)
+					maxAge: 31536000
 				}
 			)
 		}
