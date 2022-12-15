d077eca9de503b98dc5d50fef0bc462c9e6cf047
Rienk <rienkfidder@gmail.com>
2022-12-15 22:28:07 +0100

    Fixed sidebar

--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -44,7 +44,7 @@
 	</Header>
 </div>
 {#if innerWidth < 1056}
-	<SideNav bind:isOpen={$open}>
+	<SideNav bind:isOpen={$open} class="sidenav">
 		<SideNavItems>
 			<SideNavLink href="/" text="Home" />
 			<SideNavLink href="/" text="Info" />
@@ -65,5 +65,14 @@
 	/* Since the header has a fixed position, we wrap it in a simple div with fixed height to prevent hiding data. */
 	.header-wrapper {
 		height: 48px;
+	}
+
+	/* The normal z-index was 8000, messing with the modal. 100 fixes this but stull stays above most of the items */
+	:global(.sidenav) {
+		z-index: 100 !important;
+	}
+	/* This is the shadow, which had the same problem */
+	:global(.bx--side-nav__overlay-active) {
+		z-index: 99 !important;
 	}
 </style>
