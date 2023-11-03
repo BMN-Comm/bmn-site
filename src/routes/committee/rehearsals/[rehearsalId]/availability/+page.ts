9013c3d2724173ac8a9d25ccc88069385d1d9396
Tim Griffioen <timgriffioen2000@gmail.com>
2023-11-03 14:08:17 +0100

    Fixed bug

--- src/routes/committee/rehearsals/[rehearsalId]/availability/+page.ts
+++ src/routes/committee/rehearsals/[rehearsalId]/availability/+page.ts
@@ -17,16 +17,18 @@ export const load: PageLoad = async ({ params }) => {
 	)
 	const allAvailability = (await getDocs(availabilityQuery)).docs
 
-	const userAvailabilities: { name: string; availability: availability | undefined }[] = users.map(
-		(user) => {
+	const userAvailabilities: { name: string; availability: availability | undefined }[] = users
+		.filter(
+			(user) => user.id !== 'Om9orRRBrIkeXhHTHELj' // Filter out admin account as it is not a user
+		)
+		.map((user) => {
 			return {
 				name: user.name,
 				availability: allAvailability.find((x) => x.ref.parent.parent!.id === user.id)?.data() as
 					| availability
 					| undefined
 			}
-		}
-	)
+		})
 
 	return { userAvailabilities }
 }
