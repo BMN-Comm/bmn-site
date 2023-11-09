85e041f41a3aaae3016d1f2ca85d34c01bcc463a
Tim Griffioen <timgriffioen2000@gmail.com>
2023-11-09 17:58:56 +0200

    Merge errors

--- src/routes/committee/scheduling/+page.ts
+++ src/routes/committee/scheduling/+page.ts
@@ -1,7 +1,7 @@
 import { db, verifyUserLoggedIn } from '$lib/firebase/client/firebase'
 import { GetMusisciansThatPlaySongs } from '$lib/firebase/client/firestore/musicians'
 import { getSongs } from '$lib/firebase/client/firestore/songs'
-import { GetAllUsers } from '$lib/firebase/client/firestore/users'
+import { getUsers } from '$lib/firebase/client/firestore/users'
 import { editionId, type edition } from '$lib/types/domain/edition'
 import { doc, getDoc } from 'firebase/firestore'
 import type { PageLoad } from './$types'
@@ -20,7 +20,7 @@ export const load: PageLoad = async () => {
 
 	const musiciansForSongs = GetMusisciansThatPlaySongs(editionSongIds)
 
-	const users = GetAllUsers()
+	const users = getUsers()
 
 	return { songs, musiciansForSongs, users }
 }
