5bf951eefc44309a7c4daad26fa7ff7abcb38570
Tim Griffioen <timgriffioen2000@gmail.com>
2023-11-24 23:09:09 +0100

    Updated logo and icon

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -22,7 +22,7 @@
 	<svelte:component this={Carousel} autoplay>
 		{#each carouselPhotos as foto}
 			<div class="carouselDiv">
-				<img class="BMNLogoCarousel" src="BMN2023.png" alt="BMNLogo" />
+				<img class="BMNLogoCarousel" src="BMN2024.png" alt="BMNLogo" />
 				<img class="carouselPhoto" src="carouselPhotos/{foto}" alt={foto} />
 			</div>
 		{/each}
