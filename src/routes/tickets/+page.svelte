8466997243dea03a93018c7f1f5c6172202725f2
Busenuryilmaz <busenuryilmaz0555@gmail.com>
2026-04-28 15:33:14 +0200

    Updates tickets page to Yippeee ticketsale

--- src/routes/tickets/+page.svelte
+++ src/routes/tickets/+page.svelte
@@ -5,14 +5,14 @@
 
 <div class="tickText">
 	<h1><strong>TICKET SALE</strong></h1>
-	<h2>The ticket sale is closed!</h2>
-	<h2>To be notified, fill in the interest form <a href="https://forms.gle/jXuT1JZXQDBLTspv9">here</a> to be notified when the ticket sale opens.</h2>
+	<h2>The ticket sale HAS STARTED!</h2>
+	<h2> <a href="https://betamusicnight.com/tickets">GET YOUR TICKETS NOW!</a> </h2> 
 	<h2>Do you want to keep up with all BMN news?<br> Follow us on instagram!</h2>
 	<a href= https://instagram.com/betamusicnight> 
 		<img class="socialImg" width="100px" src="socials/instagram.png" alt="InstagramIcon" />
 	</a>
 </div>
-
+//FIX LINKKKKKKKKK
 <style>
 	.tickText {
 		margin: 10% 25%;
